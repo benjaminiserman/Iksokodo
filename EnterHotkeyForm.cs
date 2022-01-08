b7f516dca954cecd61bdb384512475292b23f84f
Benjamin E. Iserman <winggar1228@gmail.com>
2022-01-08 15:17:43 -0600

    add hotkey selector window

---
+++ EnterHotkeyForm.cs
@@ -0,0 +1,26 @@
+﻿namespace Iksokodo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+public partial class EnterHotkeyForm : Form
+{
+	public EnterHotkeyForm()
+	{
+		InitializeComponent();
+	}
+
+	private void SubmitButton_Click(object sender, EventArgs e)
+	{
+		HotkeyManager.UpdateHotkey(HotkeyTextBox.Text);
+		Close();
+	}
+
+	public Control GetHotkeyTextbox() => HotkeyTextBox;
+}
--- HotKeyManager.cs
+++ HotKeyManager.cs
@@ -10,21 +10,45 @@ using WK.Libraries.HotkeyListenerNS;
 internal static class HotkeyManager
 {
 	static HotkeyListener _listener;
+	static Action<object, EventArgs> _action;
+	static Hotkey _hotkey;
 
     public static void RegisterHotkey(Action<object, EventArgs> action)
 	{
 		_listener = new();
+		_action = action;
 
-		Hotkey toggleKey = Program.Config.ToggleHotkey;
+		_hotkey = Program.Config.ToggleHotkey;
 
-		_listener.Add(toggleKey);
+		_listener.Add(_hotkey);
 
 		_listener.HotkeyPressed += (_, e) =>
 		{
-			if (e.Hotkey == toggleKey)
+			if (e.Hotkey == _hotkey)
 			{
-				action(null, null);
+				_action(null, null);
 			}
 		};
+	}
+
+	public static void EnableSelector(Control control) => new HotkeySelector().Enable(control);
+
+	public static void UpdateHotkey(string s)
+	{
+		Hotkey newHotkey;
+
+		try
+		{
+			newHotkey = new(s);
+		}
+		catch (Exception ex)
+		{
+			MessageBox.Show($"Change hotkey failed.\n{ex}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			throw;
+		}
+
+		_listener.Update(_hotkey, newHotkey);
+
+		_hotkey = newHotkey;
 	}
 }
